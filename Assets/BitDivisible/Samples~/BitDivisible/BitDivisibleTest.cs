31fb1e894afcb0c1ef7e696530694fd2c639bca2
riru-ruribe <riru.ruribe@gmail.com>
2024-03-16 17:18:15 +0900

    サンプル追加

---
+++ Assets/BitDivisible/Samples~/BitDivisible/BitDivisibleTest.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace BitDivisible.Samples
+{
+    [BitDivisible]
+    partial class Data
+    {
+        [BitDivisibleField(typeof(int), 3, "A")]
+        [BitDivisibleField(typeof(bool), 1, "B")]
+        public ulong i;
+    }
+
+    class BitDivisibleTest : MonoBehaviour
+    {
+        readonly ulong[] ary = { 1, 2, 4, 8, 16, };
+
+        [ContextMenu("Test")]
+        void Start()
+        {
+            var data = new Data();
+            foreach (var i in ary)
+            {
+                data.i = i;
+                Debug.Log($"A:{data.A}, B:{data.B}");
+            }
+        }
+    }
+}

c752bfd0d70aff463e45cf9ccd0405c1ff9bfae9
riru-ruribe <riru.ruribe@gmail.com>
2024-03-16 16:59:37 +0900

    SourceGenerator追加

---
+++ BitDivisibleGenerator/BitDivisibleGenerator/DiagnosticDescriptors.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace BitDivisibleGenerator;
+
+public static class DiagnosticDescriptors
+{
+    const string Category = "BitDivisible";
+
+    public static readonly DiagnosticDescriptor E0001 = new(
+        id: Category + nameof(E0001),
+        title: "invalid accessibility",
+        messageFormat: "'public' or 'protected' or 'internal' or 'private' is allowed.",
+        category: Category,
+        defaultSeverity: DiagnosticSeverity.Error,
+        isEnabledByDefault: true
+    );
+
+    public static readonly DiagnosticDescriptor E0002 = new(
+        id: Category + nameof(E0002),
+        title: "invalid syntax",
+        messageFormat: "'partial' class required.",
+        category: Category,
+        defaultSeverity: DiagnosticSeverity.Error,
+        isEnabledByDefault: true
+    );
+
+    public static readonly DiagnosticDescriptor W0001 = new(
+        id: Category + nameof(W0001),
+        title: "invalid attribute",
+        messageFormat: "'BitDivisible' attribute is not needed because fields does not have any attributes.",
+        category: Category,
+        defaultSeverity: DiagnosticSeverity.Warning,
+        isEnabledByDefault: true
+    );
+}
